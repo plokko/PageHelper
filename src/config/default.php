0944a6c95466a8e09880eb974d7ff22624b81c8d
Plokko <plo_koon@email.it>
2016-03-29 19:39:39 +0200

    updated config values

--- src/config/default.php
+++ src/config/default.php
@@ -1,3 +1,10 @@
 <?php
 return [
+    'charset'=>'utf-8',
+    'sections'=>[
+        'htmlTagAttributes'=>'html.attributes',
+        'bodyTagAttributes'=>'html.attributes',
+        'head'=>'page.head',
+        'footerScripts'=>null,
+    ],
 ];
--- src/config/example.php
+++ src/config/example.php
@@ -3,6 +3,8 @@
 return [
     'title'=>'My default page title',
     'description'=>'My default page description',
+    'keywords'=>['Laravel','PageHelper','default','keywords'],//Array or string
+
     //Set sections//
     'sections'=>[
         'htmlTagAttributes'=>'html.attributes',
@@ -11,19 +13,27 @@ return [
         'footerScripts'=>null,
     ],
 
-    //'icon'=>'icon_url.png',
-
     'charset'=>'utf-8',
-    //'icon'=>'favicon.ico', /*or as array: ['icon.png','icon.ico',..]*/
+
+    //'icon'=>'favicon.ico',
+    // OR as an array:
+    // icon'=>['icon.png','icon.ico',],
+
     'meta'=>[
         'tags'=>[
-            //'tag-name'=>'value', /*OR*/ 'tag-name1'=>['content'=>'value','attr'=>'attr-value'],
+            //'tag-name'=>'value',
+            // OR for custom tag attributes
+            //'tag-name1'=>['content'=>'value','attr'=>'attr-value'],
         ],
         'http-equiv'=>[
-            //'tag-name'=>'value', /*OR*/ 'tag-name1'=>['content'=>'value','attr'=>'attr-value'],
+            //'tag-name'=>'value',
+            // OR for custom tag attributes
+            // 'tag-name1'=>['content'=>'value','attr'=>'attr-value'],
         ],
         'og'=>[
-            //'og:propriety'=>'value', /*OR*/ 'og:propriety'=>['value1','value2']
+            //'og:propriety'=>'value',
+            // OR for multiple values
+            //'og:propriety'=>['value1','value2']
         ],
     ],
     'script'=>[
